3cc60e1ec88274962c9e46027cbdf8cec3c03120
cristianiniguez <felizpc1@gmail.com>
2021-04-16 20:04:02 -0400

    variables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
+const val PI = 3.1416
+
 fun main(args: Array<String>) {
     println("Hello World!")
+    var dinero = 10
+    println(dinero) // 10
+    dinero = 5
+    println(dinero) // 5
+
+    val nombre = "Cristian"
+    println(nombre) // Cristian
+
+    println(PI) // 3.1416
 }
