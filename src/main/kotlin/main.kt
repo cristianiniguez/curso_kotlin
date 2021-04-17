c58b40d07c9a322edfd431994d7b636742f812c0
cristianiniguez <felizpc1@gmail.com>
2021-04-17 17:55:07 -0400

    bucles: while y do while

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,28 +1,14 @@
 fun main(args: Array<String>) {
-    val colorName = "Carmesí"
+    var counter = 10
 
-    when (colorName) {
-        "Amarillo" -> println("El amarillo es el color de la alegría")
-        "Rojo", "Carmesí" -> println("Este color simboliza el calor")
-        else -> println("Error, no tengo información de este color")
+    while (counter > 0) {
+        println("El valor del contador es $counter")
+        counter--
     }
 
-    val code = 200
-
-    when (code) {
-        in 200..299 -> println("Todo ha ido bien")
-        in 400..500 -> println("Algo ha fallado")
-        else -> println("Código desconocido, algo ha fallado")
-    }
-
-    val shoesSize = 41
-
-    val message = when (shoesSize) {
-        41,43 -> "Tenemos disponible"
-        42,44 -> "Casi no nos quedan"
-        45 -> "Lo siento, no tenemos disponible"
-        else -> "Estos zapatos solo vienen en tallas 41, 42, 43, 44 y 45"
-    }
-
-    println(message)
+    do {
+        println("Generando numero aleatorio ...")
+        val randomNumber = (0..100).random()
+        println("El número generado es $randomNumber")
+    } while (randomNumber > 50)
 }
