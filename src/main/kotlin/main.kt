310f3b4e1cc663e4a0b524a2cbca18df06f728fb
cristianiniguez <felizpc1@gmail.com>
2021-04-16 20:31:53 -0400

    tipos de variables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,13 +2,29 @@ const val PI = 3.1416
 
 fun main(args: Array<String>) {
     println("Hello World!")
-    var dinero = 10
-    println(dinero) // 10
-    dinero = 5
-    println(dinero) // 5
+    var money = 10
+    println(money) // 10
+    money = 5
+    println(money) // 5
 
-    val nombre = "Cristian"
-    println(nombre) // Cristian
+    val name = "Cristian"
+    println(name) // Cristian
 
     println(PI) // 3.1416
+
+    val boolean = true
+    val longNumber = 3L
+    val doubleNumber = 2.7282
+    val floatNumber = 1.1f
+
+    val first = 20
+    val second = 10
+
+    val third = first - second
+    println(third) // 10
+
+    val lastName = "Iñiguez"
+    val firstName = "Cristian"
+    val fullName = "Mi nombre es $firstName $lastName"
+    println(fullName) // Mi nombre es Cristian Iñiguez
 }
