c2c6b93f978794f2871e57afb68137dc9cfb3ef8
cristianiniguez <felizpc1@gmail.com>
2021-04-16 21:58:39 -0400

    estructuras de control: when

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,27 @@
 fun main(args: Array<String>) {
-    val name = "Dario"
+    val colorName = "Carmesí"
 
-    if (name.isNotEmpty()) println("El largo de nuestra variable es ${name.length}") else println("Error, la variable está vacía")
+    when (colorName) {
+        "Amarillo" -> println("El amarillo es el color de la alegría")
+        "Rojo", "Carmesí" -> println("Este color simboliza el calor")
+        else -> println("Error, no tengo información de este color")
+    }
 
-    val message: String = if (name.length > 4) {
-        "Tu nombre es largo"
-    } else if (name.isEmpty()) {
-        "El nombre está vacío"
-    } else {
-        "Tienes un nombre corto"
+    val code = 200
+
+    when (code) {
+        in 200..299 -> println("Todo ha ido bien")
+        in 400..500 -> println("Algo ha fallado")
+        else -> println("Código desconocido, algo ha fallado")
+    }
+
+    val shoesSize = 41
+
+    val message = when (shoesSize) {
+        41,43 -> "Tenemos disponible"
+        42,44 -> "Casi no nos quedan"
+        45 -> "Lo siento, no tenemos disponible"
+        else -> "Estos zapatos solo vienen en tallas 41, 42, 43, 44 y 45"
     }
 
     println(message)
