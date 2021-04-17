8f9f7d97a08424eaa0f0c2a2f41a06ec546bedcd
cristianiniguez <felizpc1@gmail.com>
2021-04-16 21:23:24 -0400

    estructuras de control: if

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,15 @@
-const val PI = 3.1416
-
 fun main(args: Array<String>) {
-    println("Hello World!")
-    var money = 10
-    println(money) // 10
-    money = 5
-    println(money) // 5
+    val name = "Dario"
 
-    val name = "Cristian"
-    println(name) // Cristian
+    if (name.isNotEmpty()) println("El largo de nuestra variable es ${name.length}") else println("Error, la variable está vacía")
 
-    println(PI) // 3.1416
+    val message: String = if (name.length > 4) {
+        "Tu nombre es largo"
+    } else if (name.isEmpty()) {
+        "El nombre está vacío"
+    } else {
+        "Tienes un nombre corto"
+    }
 
-    val boolean = true
-    val longNumber = 3L
-    val doubleNumber = 2.7282
-    val floatNumber = 1.1f
-
-    val first = 20
-    val second = 10
-
-    val third = first - second
-    println(third) // 10
-
-    val lastName = "Iñiguez"
-    val firstName = "Cristian"
-    val fullName = "Mi nombre es $firstName $lastName"
-    println(fullName) // Mi nombre es Cristian Iñiguez
+    println(message)
 }
