9c44b16b21574592fcac6133bc1d4593485f508b
cristianiniguez <felizpc1@gmail.com>
2021-04-17 20:03:39 -0400

    listas

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,33 @@
 fun main(args: Array<String>) {
-    var name: String? = null
-    val nameChars: Int = name?.length ?: 0
-    println(nameChars) // 0
+    val namesList = listOf("Juan", "Enrique", "Camila")
+    println(namesList) // [Juan, Enrique, Camila]
+
+    val voidList = mutableListOf<String>()
+    println(voidList) // []
+
+    voidList.add("Juan")
+    println(voidList) // [Juan]
+
+    val valueUsingGet = voidList.get(0)
+    println(valueUsingGet) // Juan
+
+    val valueUsingOperator = voidList[0]
+    println(valueUsingOperator) // Juan
+
+    val firstValue: String? = namesList.firstOrNull()
+    println(firstValue) // Juan
+
+    voidList.removeAt(0)
+    println(voidList) // []
+
+    voidList.add("Enrique")
+    voidList.add("Luis")
+
+    println(voidList) // [Enrique, Luis]
+    voidList.removeIf { chars -> chars.length > 4 }
+    println(voidList) // [Luis]
+
+    val myArray = arrayOf(1, 2, 3)
+    println(myArray) // [Ljava.lang.Integer;@483bf400
+    println(myArray.toList()) // [1, 2, 3]
 }
