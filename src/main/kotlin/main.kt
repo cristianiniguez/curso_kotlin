67414cb02723827318a22233a99c2e48ca09e8ae
cristianiniguez <felizpc1@gmail.com>
2021-04-18 14:12:57 -0400

    tipos de parametros en las funciones

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,7 @@
 fun main(args: Array<String>) {
-    val randomPhrase = "En Platzi nunca paramos de aprender".randomCase()
-    printPhrase(randomPhrase)
+    printName(firstName = "Cristian", lastName = "Iñiguez") // My name is Cristian  Iñiguez
 }
 
-fun printPhrase(phrase: String): Unit {
-    println("Tu frase es: $phrase")
-}
-
-fun String.randomCase(): String {
-    val randomNumber = 0..99
-    val randomResult = randomNumber.random()
-    return if (randomResult.rem(2) == 0) {
-        this.toUpperCase()
-    } else {
-        this.toLowerCase()
-    }
+fun printName(firstName: String, secondName: String = "", lastName: String) {
+    println("My full name is $firstName $secondName $lastName")
 }
