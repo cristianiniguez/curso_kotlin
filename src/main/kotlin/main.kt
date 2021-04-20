b4ac44026294dbe010c977ba0e5718581b20b21a
cristianiniguez <felizpc1@gmail.com>
2021-04-20 19:57:47 -0400

    scope functions: run

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,9 @@
 fun main(args: Array<String>) {
-    val colors = listOf("Blue", "Yellow", "Red")
-    with(colors) {
-        println("Nuestros colores son $this") // Nuestros colores son [Blue, Yellow, Red]
-        println("Esta lista tiene $size color(es)") // Esta lista tiene 3 color(es)
-    }
+    val mobiles = mutableListOf("Pixel 1", "Pixel 2", "Huawei 9", "Xiaomi A3")
+        .run {
+            removeIf { mobile -> mobile.contains("Pixel") }
+            this
+        }
+
+    print(mobiles) // [Huawei 9, Xiaomi A3]
 }
