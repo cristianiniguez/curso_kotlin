bc90c02d149d71f346881084174e86da8bc3f9ea
cristianiniguez <felizpc1@gmail.com>
2021-04-20 19:44:48 -0400

    scope functions: let

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,7 @@
 fun main(args: Array<String>) {
-    val initialValueLength = superFunction(initialValue = "Hello") { value -> value.length }
-    print(initialValueLength) // 5
+    var name: String? = null
+    name?.let { value -> println("El nombre no es nulo. Es $value") }
 
-    val lambda: () -> String = functionInception("Felix")
-    val lambdaValue: String = lambda()
-    println(lambdaValue) // Hola desde la lambda Felix
-}
-
-fun superFunction(initialValue: String, block: (String) -> Int): Int {
-    return block(initialValue)
-}
-
-fun functionInception(name: String): () -> String {
-    return { "Hola desde la lambda $name" }
+    name = "Cristian"
+    name?.let { value -> println("El nombre no es nulo. Es $value") }
 }
