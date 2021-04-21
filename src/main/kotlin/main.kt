52b76ee6e38d74ab31e0e08d88454ccd8fd4a423
cristianiniguez <felizpc1@gmail.com>
2021-04-20 20:05:26 -0400

    scope functions: apply

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,14 @@
 fun main(args: Array<String>) {
     val mobiles = mutableListOf("Pixel 1", "Pixel 2", "Huawei 9", "Xiaomi A3")
-        .run {
+        .apply {
             removeIf { mobile -> mobile.contains("Pixel") }
-            this
         }
 
     print(mobiles) // [Huawei 9, Xiaomi A3]
+
+    val colors: MutableList<String>? = null
+    colors?.apply {
+        println("Nuestros colores son $this")
+        println("La cantidad de colores es $size")
+    }
 }
