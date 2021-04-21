01b4d1e8b9afa8c43360950ff6102047654f9f5a
cristianiniguez <felizpc1@gmail.com>
2021-04-20 20:22:03 -0400

    proyecto bola 8 magica: creando el menu

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,36 @@
+// Declaramos las variables de nuestras respuestas
+const val RESPUESTA_AFIRMATIVA = "✔"
+const val RESPUESTA_NEGATIVA = "✘"
+const val RESPUESTA_DUDOSA = "?"
+
+// Unimos las respuestas con los valores
+val respuestas = mapOf(
+    "Si" to RESPUESTA_AFIRMATIVA,
+    "Es cierto" to RESPUESTA_AFIRMATIVA,
+    "Totalmente" to RESPUESTA_AFIRMATIVA,
+    "Sin duda" to RESPUESTA_AFIRMATIVA,
+    "Pregunta en otro momento" to RESPUESTA_DUDOSA,
+    "No puedo decirte en este momento" to RESPUESTA_DUDOSA,
+    "Puede que si o puede que no" to RESPUESTA_DUDOSA,
+    "No va a suceder" to RESPUESTA_NEGATIVA,
+    "No cuentes con ello" to RESPUESTA_NEGATIVA,
+    "Definitivamente no" to RESPUESTA_NEGATIVA,
+    "No lo creo" to RESPUESTA_NEGATIVA,
+)
+
 fun main(args: Array<String>) {
-    val mobiles = mutableListOf("Pixel 1", "Pixel 2", "Huawei 9", "Xiaomi A3")
-        .also {
-            list -> println("Valor original: $list") // Valor original: [Pixel 1, Pixel 2, Huawei 9, Xiaomi A3]
-        }.asReversed()
+    println("Hola, soy tu Bola 8 mágica creada en Kotlin")
+    println("¿Cuál de estas opciones deseas realizar?")
+    println("1. Realizar una pregunta")
+    println("2. Revisar todas las respuestas")
+    println("3. Salir")
 
-    println(mobiles) // [Xiaomi A3, Huawei 9, Pixel 2, Pixel 1]
+    val valorIngresado = readLine()
 
-
+    when(valorIngresado) {
+        "1" -> realizarPregunta()
+        "2" -> mostrarRespuestas()
+        "3" -> salir()
+        else -> mostrarError()
+    }
 }
